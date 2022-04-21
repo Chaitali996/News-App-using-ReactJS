69055fe9b6966b3c7e338f9944f5f7b89463183b
Chaitali996 <chaitalidahire2001@gmail.com>
2022-04-21 14:35:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./styles.css";
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios';
+import News from './Components/News.js'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [text, setText] = useState("business")
+  const [news, setNews] = useState( [] )
 
-export default App;
+  const changeText = (event) => {
+    setText(event.target.value);
+  }
+  const fetchNews = async () => {
+    let response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${text}&apiKey=e037318ca9924fd5ac3a73c9d427c2c0`)
+    console.log(response.data.articles);
+    setNews(response.data.articles)
+  }
+
+  return (
+    <>
+      <h1 className="text-center my-2">News App using React JS</h1>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="text"
+              className="form-control"
+              value={text}
+              onChange={changeText}
+            />
+          </div>
+          <div className="col-3">
+            <button className="btn btn-primary" onClick={fetchNews}>
+              Fetch News
+            </button>
+          </div>
+        </div>
+      </div>
+      {
+          news?.length > 0 ? (<News data={news} />) : (<h3 className="text-center">no news</h3>)
+      }
+      
+    </>
+  )
+}
+export default App
---
+++ src/Components/News.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function News({ data }) {
+  return (
+    <div className="container my-3">
+      <div className="row">
+        {data.map((value) => {
+          return (
+            <div className="col">
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  src={value.urlToImage}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{value.title}</h5>
+                  <p className="card-text">{value.description}</p>
+                  <a
+                    href={value.url}
+                    target="#"
+                    className="btn btn-primary">
+                    Main source
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default News;
